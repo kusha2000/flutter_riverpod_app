6016b1aea61175f7b9c8c2eaba5326f5eff7abb9
kusha2000 <kushanandarawewa1@gmail.com>
2026-05-06 20:50:50 +0530

    Create Post model

--- lib/data/models/post.dart
+++ lib/data/models/post.dart
@@ -1,3 +1,38 @@
 class Post {
-  
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  const Post({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'] as int,
+      userId: json['userId'] as int,
+      title: json['title'] as String,
+      body: json['body'] as String,
+    );
+  }
+
+  Post copyWith({String? title, String? body}) {
+    return Post(
+      id: id,
+      userId: userId,
+      title: title ?? this.title,
+      body: body ?? this.body,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || (other is Post && other.id == id);
+
+  @override
+  int get hashCode => id.hashCode;
 }
